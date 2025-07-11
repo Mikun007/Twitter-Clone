9d21aebe765a6e3abffe8b25b4cdba7549af6ab6
Budhadev Das <mikundas@outlook.com>
2025-07-11 20:16:35 +0530

    navbar foundation

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,8 +4,26 @@ const Nav = () => {
     return ( 
         <>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/Profile">Profile</Link>
+                <div>
+                    <Link to="/">Profile Logout</Link>
+                </div>
+                <div className="upperNav" style={{display: "flex", flexDirection: "column", overflowY: "scroll"}}>
+                    <Link to="/"><span><p className="textNav">Home</p></span></Link>
+                    <Link to="/"><span><p className="textNav">Explore</p></span></Link>
+                    <Link to="/"><span><p className="textNav">Notification</p></span></Link>
+                    <Link to="/"><span><p className="textNav">Messages</p></span></Link>
+                    <Link to="/"><span><p className="textNav">Grok</p></span></Link>
+                    <Link to="/"><span><p className="textNav">Communities</p></span></Link>
+                    <Link to="/"><span><p className="textNav">Premium</p></span></Link>
+                    <Link to="/Profile"><span><p className="textNav">Profile</p></span></Link>
+                    <Link to="/"><span><p className="textNav">More</p></span></Link>
+                </div>
+
+                <button>Post</button>
+                <div className="logout">
+                    <Link to="/">Profile Logout</Link>
+                </div>
+
             </nav>
         </>
      );
